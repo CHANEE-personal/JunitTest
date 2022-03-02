21906a0dd29ff0b8b93a1419b0c294e3bc22484d
Cho Chan Hee <cksgml4424@gmail.com>
2022-03-02 22:06:57 +0900

    junit test commit

---
+++ src/test/java/com/example/studyTest.java
@@ -0,0 +1,42 @@
+package com.example;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class studyTest {
+
+    @Test
+    @DisplayName("스터디 만들기")
+    public void create_new_study() {
+        Study study = new Study();
+        assertNotNull(study);
+        System.out.println("create");
+    }
+
+    @Test
+    void create_new_study_again() {
+        System.out.println("create1");
+    }
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("before all");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("after all");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("Before Each");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("After Each");
+    }
+}
