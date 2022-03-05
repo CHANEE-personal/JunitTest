5be842d5016597a1273869fc1a8a8b7ab15f08bc
Cho Chan Hee <cksgml4424@gmail.com>
2022-03-05 13:55:45 +0900

    junit test commit

--- src/test/java/com/example/studyTest.java
+++ src/test/java/com/example/studyTest.java
@@ -2,7 +2,10 @@ package com.example;
 
 import org.junit.jupiter.api.*;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.lang.reflect.Executable;
+import java.util.function.Supplier;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class studyTest {
@@ -10,9 +13,14 @@ class studyTest {
     @Test
     @DisplayName("스터디 만들기")
     public void create_new_study() {
-        Study study = new Study();
-        assertNotNull(study);
-        System.out.println("create");
+        Study study = new Study(-10);
+
+        assertAll(
+                () -> assertNotNull(study),
+                () -> assertEquals(StudyStatus.DRAFT, study.getStatus(),
+                    () -> "스터디를 처음 만들면 " + StudyStatus.DRAFT + "상태다."),
+                () -> assertTrue(study.getLimit() > 0, "스터디 최대 참석 가능 인원은 0보다 커야 한다.")
+        );
     }
 
     @Test
