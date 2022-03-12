5d50769fbc878519584f3a749981e774263db171
Cho Chan Hee <cksgml4424@gmail.com>
2022-03-12 17:15:42 +0900

    commit

--- src/test/java/com/example/studyTest.java
+++ src/test/java/com/example/studyTest.java
@@ -1,19 +1,43 @@
 package com.example;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 
 import java.lang.reflect.Executable;
 import java.time.Duration;
 import java.util.function.Supplier;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+import static org.junit.jupiter.api.Assumptions.assumingThat;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class studyTest {
 
     @Test
     @DisplayName("스터디 만들기")
+    @EnabledOnOs({ OS.MAC, OS.LINUX })
     public void create_new_study() {
+
+        String test_env = System.getenv("TEST_ENV");
+        System.out.println("local");
+        Study actual = new Study(100);
+        assertThat(actual.getLimit()).isGreaterThan(0);
+
+//        assumingThat("LOCAL".equalsIgnoreCase(test_env), () -> {
+//            System.out.println("local");
+//            Study actual = new Study(100);
+//            assertThat(actual.getLimit()).isGreaterThan(0);
+//        });
+//
+//        assumingThat("chanee".equalsIgnoreCase(test_env), () -> {
+//            System.out.println("chanee");
+//            Study actual = new Study(100);
+//            assertThat(actual.getLimit()).isGreaterThan(0);
+//        });
+
         Study study = new Study(-10);
 
 //        assertAll(
