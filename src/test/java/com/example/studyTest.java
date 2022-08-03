f758dbf988331099c3e81e6a20ebcbe7a196d63d
CHO CHAN HEE <cksgml4424@gmail.com>
2022-08-03 12:50:21 +0900

    juint 초기 설정 커밋

--- src/test/java/com/example/studyTest.java
+++ src/test/java/com/example/studyTest.java
@@ -13,6 +13,39 @@ import static org.junit.jupiter.api.Assertions.*;
 class studyTest {
 
     @Test
+    void create() {
+        Study study = new Study(100);
+        assertNotNull(study);
+        System.out.println("create");
+    }
+
+    @Test
+    @Disabled
+    void create1() {
+        System.out.println("create1");
+    }
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("before all");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("after all");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("before each");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("after each");
+    }
+
+    @Test
     @DisplayName("스터디 만들기")
     @EnabledOnOs({ OS.MAC, OS.LINUX })
     public void create_new_study() {
@@ -49,30 +82,5 @@ class studyTest {
             new Study(10);
             Thread.sleep(300);
         });
-    }
-
-    @Test
-    void create_new_study_again() {
-        System.out.println("create1");
-    }
-
-    @BeforeAll
-    static void beforeAll() {
-        System.out.println("before all");
-    }
-
-    @AfterAll
-    static void afterAll() {
-        System.out.println("after all");
-    }
-
-    @BeforeEach
-    void beforeEach() {
-        System.out.println("Before Each");
-    }
-
-    @AfterEach
-    void afterEach() {
-        System.out.println("After Each");
     }
 }
