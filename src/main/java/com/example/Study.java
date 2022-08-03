5666bc8622f0d295c5d8d01ab964de35392bb16d
CHO CHAN HEE <cksgml4424@gmail.com>
2022-08-03 13:42:59 +0900

    juint assertion commit

--- src/main/java/com/example/Study.java
+++ src/main/java/com/example/Study.java
@@ -8,9 +8,9 @@ public class Study {
 
     public Study(int limit) {
 
-//        if (limit < 0) {
-//            throw new IllegalArgumentException("limit은 0보다 커야한다.");
-//        }
+        if (limit < 0) {
+            throw new IllegalArgumentException("limit은 0보다 커야한다.");
+        }
         this.limit = limit;
     }
 
--- src/test/java/com/example/studyTest.java
+++ src/test/java/com/example/studyTest.java
@@ -13,15 +13,35 @@ import static org.junit.jupiter.api.Assertions.*;
 class studyTest {
 
     @Test
-    void create() {
+    @DisplayName("스터디 만들기")
+    void create_new_study() {
+        Study actual = new Study(10);
+        assertThat(actual.getLimit()).isGreaterThan(0);
+
+        assertTimeout(Duration.ofMillis(100), () -> {
+            new Study(10);
+            Thread.sleep(300);
+        });
+
+        IllegalArgumentException exception =
+                assertThrows(IllegalArgumentException.class, () -> new Study(-10));
+
+        String message = exception.getMessage();
+        assertEquals("limit은 0보다 커야한다.", message);
+
         Study study = new Study(100);
-        assertNotNull(study);
-        System.out.println("create");
+
+        assertAll(
+                () -> assertNotNull(study),
+                () -> assertEquals(StudyStatus.DRAFT, study.getStatus(),
+                        () -> "스터디를 처음 만들면 " + StudyStatus.DRAFT + " 상태다."),
+                () -> assertTrue(study.getLimit() > 0, "스터디 최대 참석 가능 인원은 0보다 커야한다.")
+        );
     }
 
     @Test
-    @Disabled
-    void create1() {
+    @DisplayName("스터디 만들기")
+    void create_new_study_again() {
         System.out.println("create1");
     }
 
@@ -43,44 +63,5 @@ class studyTest {
     @AfterEach
     void afterEach() {
         System.out.println("after each");
-    }
-
-    @Test
-    @DisplayName("스터디 만들기")
-    @EnabledOnOs({ OS.MAC, OS.LINUX })
-    public void create_new_study() {
-        String test_env = System.getenv("TEST_ENV");
-        System.out.println("local");
-        Study actual = new Study(100);
-        assertThat(actual.getLimit()).isGreaterThan(0);
-
-//        assumingThat("LOCAL".equalsIgnoreCase(test_env), () -> {
-//            System.out.println("local");
-//            Study actual = new Study(100);
-//            assertThat(actual.getLimit()).isGreaterThan(0);
-//        });
-//
-//        assumingThat("chanee".equalsIgnoreCase(test_env), () -> {
-//            System.out.println("chanee");
-//            Study actual = new Study(100);
-//            assertThat(actual.getLimit()).isGreaterThan(0);
-//        });
-
-        Study study = new Study(-10);
-
-//        assertAll(
-//                () -> assertNotNull(study),
-//                () -> assertEquals(StudyStatus.DRAFT, study.getStatus(),
-//                    () -> "스터디를 처음 만들면 " + StudyStatus.DRAFT + "상태다."),
-//                () -> assertTrue(study.getLimit() > 0, "스터디 최대 참석 가능 인원은 0보다 커야 한다.")
-//        );
-
-//        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> new Study(-10));
-//        assertEquals("limit은 0보다 커야한다.", exception.getMessage());
-
-        assertTimeout(Duration.ofMillis(100), () -> {
-            new Study(10);
-            Thread.sleep(300);
-        });
     }
 }
