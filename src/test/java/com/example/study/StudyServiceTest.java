fd98efc400c40dcf48afa40be06b0e6d3519d9bb
CHO CHAN HEE <cksgml4424@gmail.com>
2022-08-06 18:00:21 +0900

    Mock 객체 Stubbing commit

--- src/test/java/com/example/study/StudyServiceTest.java
+++ src/test/java/com/example/study/StudyServiceTest.java
@@ -1,13 +1,18 @@
 package com.example.study;
 
+import com.example.domain.Member;
+import com.example.domain.Study;
 import com.example.member.MemberService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class StudyServiceTest {
@@ -16,9 +21,39 @@ class StudyServiceTest {
 //    @Mock StudyRepository studyRepository;
 
     @Test
-    void createStudyService(@Mock MemberService memberService,
+    void createNewStudy(@Mock MemberService memberService,
                             @Mock StudyRepository studyRepository) {
         StudyService studyService = new StudyService(memberService, studyRepository);
         assertNotNull(studyService);
+
+        Member member = new Member();
+        member.setId(1L);
+        member.setEmail("cksgml@email.com");
+
+        when(memberService.findById(any()))
+                .thenReturn(Optional.of(member))
+                .thenThrow(new RuntimeException())
+                .thenReturn(Optional.empty());
+
+        Optional<Member> byId = memberService.findById(1L);
+        assertEquals("cksgml@email.com", byId.get().getEmail());
+
+        assertThrows(RuntimeException.class, () -> {
+           memberService.findById(2L);
+        });
+
+        assertEquals(Optional.empty(), memberService.findById(3L));
+
+//        assertEquals("cksgml@email.com", memberService.findById(1L).get().getEmail());
+//        assertEquals("cksgml@email.com", memberService.findById(2L).get().getEmail());
+//
+//        doThrow(new IllegalArgumentException()).when(memberService).validate(1L);
+//
+//        assertThrows(IllegalArgumentException.class, () -> {
+//            memberService.validate(1L);
+//        });
+//
+//        memberService.validate(2L);
+//        studyService.createNewStudy(1L, study);
     }
 }
